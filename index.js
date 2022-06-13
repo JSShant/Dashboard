4c86158292cea3d9779c9a51122e807602a63f1b
JSShant <joshua.shanthikumar@outlook.com>
2022-06-14 04:38:59 +1000

    Linking Table to Data from Javascript

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu__btn");
 const closeBtn = document.querySelector("#close__btn")
 
+
 menuBtn.addEventListener("click", ()=> {
     sideMenu.style.display = "block"
 })
 
 closeBtn.addEventListener("click", ()=>{
     sideMenu.style.display = "none";
+})
+
+orders.forEach(order => {
+    const tr = document.createElement("tr");
+    const  trContent =  `
+                            <td>${order.productName}</td>
+                            <td>${order.productNumber}</td>
+                            <td>${order.paymentStatus}</td>
+                            <td>${order.shipping}</td>
+                            <td>Details</td>
+                            `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr)
+    
 })
---
+++ orders.js
@@ -0,0 +1,45 @@
+const orders = [
+    {
+        productName:"Item #1",
+        productNumber:"83945",
+        paymentStatus:"Due",
+        shipping:"Pending"
+    },
+    {
+        productName:"Item #2",
+        productNumber:"253123",
+        paymentStatus:"Due",
+        shipping:"Pending"
+    },
+    {
+        productName:"Item #3",
+        productNumber:"734573",
+        paymentStatus:"Paid",
+        shipping:"Shipped"
+    },
+    {
+        productName:"Item #4",
+        productNumber:"193748",
+        paymentStatus:"Due",
+        shipping:"Pending"
+    },
+    {
+        productName:"Item #5",
+        productNumber:"019827",
+        paymentStatus:"Paid",
+        shipping:"Pending"
+    },
+    {
+        productName:"Item #6",
+        productNumber:"938476",
+        paymentStatus:"Paid",
+        shipping:"Delivered"
+    },
+    {
+        productName:"Item #7",
+        productNumber:"193381",
+        paymentStatus:"Due",
+        shipping:"Pending"
+    },
+    
+]
